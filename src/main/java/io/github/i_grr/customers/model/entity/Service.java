d804d53d389b06fe8ebe460690ce6889fb1ce562
Igor Pereira <igorpereira120@gmail.com>
2021-02-25 17:37:31 -0300

    Creation and mapping of the service entity, and adding Lombok package to the entity

--- src/main/java/io/github/i_grr/customers/model/entity/Service.java
+++ src/main/java/io/github/i_grr/customers/model/entity/Service.java
@@ -1,30 +1,33 @@
 package io.github.i_grr.customers.model.entity;
 
-import java.time.LocalDate;
+import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class Customer {
-	
+public class Service {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Column(nullable = false, length = 150)
-	private String name;
+	private String description;
 	
-	@Column(nullable = false, length = 11)
-	private String cpf;
+	@ManyToOne
+	@JoinColumn(name = "id_cliente")
+	private Customer customer;
 	
-	@Column(name = "date_register")
-	private LocalDate dateRegister;
-
+	@Column
+	private BigDecimal value;
+	
 }
