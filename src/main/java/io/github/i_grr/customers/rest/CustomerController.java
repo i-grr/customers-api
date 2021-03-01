cda37d95ed0f87ad85bdd73929aa26d11f5b46f6
Igor Pereira <igorpereira120@gmail.com>
2021-03-01 11:59:53 -0300

    Customer findById

--- src/main/java/io/github/i_grr/customers/rest/CustomerController.java
+++ src/main/java/io/github/i_grr/customers/rest/CustomerController.java
@@ -2,11 +2,14 @@ package io.github.i_grr.customers.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.i_grr.customers.model.entity.Customer;
 import io.github.i_grr.customers.model.repository.CustomerRepository;
@@ -24,6 +27,13 @@ public class CustomerController {
 		System.out.println(customer.getName());
 		System.out.println(customer.getCpf());
 		return customerRepository.save(customer);
+	}
+	
+	@GetMapping("{id}")
+	public Customer findById(@PathVariable Integer id) {
+		return customerRepository
+				.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 	
 }
