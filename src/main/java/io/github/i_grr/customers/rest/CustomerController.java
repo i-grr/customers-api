e3a9ac689202bd8c44304d0fdc71dbe5939d87b1
Igor Pereira <igorpereira120@gmail.com>
2021-03-01 12:14:09 -0300

    Customer remove

--- src/main/java/io/github/i_grr/customers/rest/CustomerController.java
+++ src/main/java/io/github/i_grr/customers/rest/CustomerController.java
@@ -2,6 +2,7 @@ package io.github.i_grr.customers.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,18 @@ public class CustomerController {
 		return customerRepository
 				.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Integer id) {
+		customerRepository
+			.findById(id)
+			.map(customer -> {
+				customerRepository.delete(customer);
+				return Void.TYPE;
+			})
+			.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 	
 }
