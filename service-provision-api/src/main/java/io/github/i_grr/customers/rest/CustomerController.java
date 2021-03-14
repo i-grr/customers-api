46592b27d5160053987a4edcb24f8ca3443ce1b4
Igor Pereira <igorpereira120@gmail.com>
2021-03-13 21:39:00 -0300

    Implementing CrossOrigin annotation in API

--- service-provision-api/src/main/java/io/github/i_grr/customers/rest/CustomerController.java
+++ service-provision-api/src/main/java/io/github/i_grr/customers/rest/CustomerController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import io.github.i_grr.customers.model.repository.CustomerRepository;
 
 @RestController
 @RequestMapping("/api/customers")
+@CrossOrigin("*")
 public class CustomerController {
 
 	@Autowired
