d6b32877d61819784c3dd0793febb57c188b2e9e
Igor Pereira <igorpereira120@gmail.com>
2021-03-15 16:49:15 -0300

    Implementing find method of service provided

--- service-provision-api/src/main/java/io/github/i_grr/serviceprovision/model/repository/ServiceProvidedRepository.java
+++ service-provision-api/src/main/java/io/github/i_grr/serviceprovision/model/repository/ServiceProvidedRepository.java
@@ -1,9 +1,18 @@
 package io.github.i_grr.serviceprovision.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import io.github.i_grr.serviceprovision.model.entity.ServiceProvided;
 
 public interface ServiceProvidedRepository extends JpaRepository<ServiceProvided, Integer> {
 
+	@Query("SELECT s FROM ServiceProvided s join s.customer c "
+			+ "WHERE UPPER(c.name) LIKE upper(:name) AND MONTH(s.date) =:month")
+	List<ServiceProvided> findByCustomerNameAndMonth(@Param("name") String name, 
+													 @Param("month") Integer month);
+	
 }
--- service-provision-api/src/main/java/io/github/i_grr/serviceprovision/rest/ServiceProvidedController.java
+++ service-provision-api/src/main/java/io/github/i_grr/serviceprovision/rest/ServiceProvidedController.java
@@ -1,14 +1,16 @@
 package io.github.i_grr.serviceprovision.rest;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -56,5 +58,12 @@ public class ServiceProvidedController {
 		return serviceProvidedRepository.save(serviceProvided);
 		
 	}
+	
+	@GetMapping
+	public List<ServiceProvided> find (@RequestParam(value = "name", required = false,  defaultValue = "") String name,
+									   @RequestParam(value = "month", required = false) Integer month) {
+		return serviceProvidedRepository.findByCustomerNameAndMonth("%" + name + "%", month);
+	}
+	
 	
 }
